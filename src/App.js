1d2d063d75818a71b64ead52c3ef18f5e8ef559b
You-now-Who <yashyadav.sr@gmail.com>
2022-08-21 08:40:38 +0530

    This is before the madness happens

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { useState } from 'react';
 import Alert from './components/Alert';
 import MicDemo from './components/MicDemo';
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -28,37 +30,39 @@ function App() {
 }
   return (
     <>
-      <Router>
-        <Navbar />
+      <ChakraProvider>
+        <Router>
+          <Navbar />
 
-        <Alert alert={alert} />
+          <Alert alert={alert} />
 
-        <div className="container my-3">
-          <Routes>
-            <Route
-              exact
-              path="/"
-              element={
-                <div>
-                  <MicDemo /> 
-                  <Home showAlert={showAlert} />{" "}
-                </div>
-              }
-            />
-            <Route exact path="/about" element={<About />} />
-            <Route
-              exact
-              path="/login"
-              element={<Login showAlert={showAlert} />}
-            />
-            <Route
-              exact
-              path="/signup"
-              element={<Signup showAlert={showAlert} />}
-            />
-          </Routes>
-        </div>
-      </Router>
+          <div className="container my-3">
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <div>
+                    <MicDemo /> 
+                    <Home showAlert={showAlert} />{" "}
+                  </div>
+                }
+              />
+              <Route exact path="/about" element={<About />} />
+              <Route
+                exact
+                path="/login"
+                element={<Login showAlert={showAlert} />}
+              />
+              <Route
+                exact
+                path="/signup"
+                element={<Signup showAlert={showAlert} />}
+              />
+            </Routes>
+          </div>
+        </Router>
+      </ChakraProvider>
     </>
   );
 }
--- src/components/AudioVisComponent.js
+++ src/components/AudioVisComponent.js
@@ -1,29 +1,133 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from "react";
+import { ReactMic } from "react-mic";
+import WaveSurfer from "wavesurfer.js";
+import { Flex, Button } from "@chakar-ui/react";
 
-import AudioVisualiser, { useMediaStream } from "react-mic-waver";
 
 
-function AudioVisComponent() {
+export default function Microphone({ pushFile }) {
+  const [record, setRecord] = useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [tempFile, setTempFile] = React.useState(null);
 
-    
+  const [playerReady, setPlayerReady] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const wavesurfer = useRef(null);
+
+
+  const waveform = useRef(null);
+
+
+  useEffect(() => {
+    if (!open || (open && !tempFile)) return;
+
+    if(!waveform.current){
+        wavesurfer.current = WaveSurfer.create({
+        container: "#wavesurfer-id",
+        waveColor: "grey",
+        progressColor: "tomato",
+        height: 140,
+        cursorWidth: 1,
+        cursorColor: "lightgrey",
+        barWidth: 2,
+        normalize: true,
+        responsive: true
+        });
+    }
+
+    wavesurfer.current.on("ready", () => {
+      setPlayerReady(true);
+    });
+
+    const handleResize = wavesurfer.current.util.debounce(() => {
+      wavesurfer.current.empty();
+      wavesurfer.current.drawBuffer();
+    }, 150);
+
+    wavesurfer.current.on("play", () => setIsPlaying(true));
+    wavesurfer.current.on("pause", () => setIsPlaying(false));
+    window.addEventListener("resize", handleResize, false);
+  }, [open, tempFile]);
+
+  useEffect(() => {
+    console.log("tempFile", tempFile);
+    if (tempFile) {
+      wavesurfer.current.load(tempFile.blobURL);
+    }
+  }, [tempFile]);
+
+  const togglePlayback = () => {
+    if (!isPlaying) {
+      wavesurfer.current.play();
+    } else {
+      wavesurfer.current.pause();
+    }
+  };
+  const stopPlayback = () => wavesurfer.current.stop();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDone = () => {
+    if (tempFile) {
+      pushFile(tempFile);
+      setTempFile(null);
+      setRecord(false);
+      setOpen(false);
+    }
+  };
+
+  const handleCancel = () => {
+    setRecord(false);
+    setTempFile(null);
+    setOpen(false);
+  };
+
+  const startRecording = () => {
+    setTempFile(null);
+    setRecord(true);
+  };
+
+  const stopRecording = () => {
+    setRecord(false);
+  };
+
+  const onData = recordedBlob => {
+    //console.log("chunk of real-time data is: ", recordedBlob);
+  };
+
+  const onStop = recordedBlob => {
+    setTempFile(recordedBlob);
+  };
   
-  const { stream, start, stop } = useMediaStream();
-
-  const toggleMic = () => stream ? stop() : start()
-
-  
+  const playAudio = () => {
+    if (waveform.current.isPlaying()) {
+      waveform.current.pause();
+    } else {
+      waveform.current.play();
+    }
+  };
+//   const classes = useStyles();
 
   return (
-    
-      <div className='container my-3'>
-       <button className="btn btn-primary" onClick={toggleMic}>
-          {stream ? 'Close Microphone' : "Open Microphone"}
-        </button>
-        <AudioVisualiser className="my-3" stream={stream} onRender={() => console.log("Render!")} style={{background: "white"}}/>
-      </div>
-    
+    <>
+        <ReactMic
+            record={record}
+            className="react-mic"
+            onStop={onStop}
+            onData={onData}
+            strokeColor="grey"
+            backgroundColor="white"
+        />
+         <Flex flexDirection="column" w="100%">
+            <div id="waveform" />
+            <Flex flexDirection="row" justifyContent="center">
+                <Button m="4" onClick={playAudio}>
+                Play / Pause
+                </Button>
+            </Flex>
+        </Flex>
+    </>
   );
-};
-  
-
-export default AudioVisComponent
+}
